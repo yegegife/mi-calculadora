386b648c31e7ec8c15b1ea2b1f34658afe7e1ad9
yegegife <yggiron@yahoo.com>
2025-03-29 19:08:52 -0500

    Se mejoran los estilos para los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CalculadoraComponent } from './calculadora/calculadora.component';
     CalculadoraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
+  operandoA: number = 0;
+  operandoB: number = 0;
+  resultado: number = 0;
 
+  // se captura el valor del operando A a trvés de evento por parametro
+  capturaOperandA(event: Event){
+    //this.operandoA = Number((<HTMLInputElement>event?.target).value);
+    this.operandoA =  Number((event.target as HTMLInputElement).value);
+  }
+  
+  // se realiza la suma de los componentes A y B
+  sumar(){
+    this.resultado = this.operandoA + this.operandoB;
+  }
 }
